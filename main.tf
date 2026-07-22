96605dc48fc1e5d6e36cda8d37cabf5c2a5be375
SMilewski <102833469+smilewski-personal@users.noreply.github.com>
2026-07-22 14:28:06 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,3 +20,14 @@ data "aws_caller_identity" "current" {}
 output "aws_account_id" {
   value = data.aws_caller_identity.current.account_id
 }
+
+resource "aws_s3_bucket" "orbit_storage" {
+  bucket_prefix = "orbit-storage-"
+
+  tags = {
+    name      = "Orbit Labs Storage"
+    managedBy = "Spacelift"
+    mission   = "First Launch"
+    project   = "Orbit-labs"
+  }
+}
