310f078bbed779e1a6fe3d55d9fc544efe9e71be
SMilewski <102833469+smilewski-personal@users.noreply.github.com>
2026-07-22 13:50:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
 resource "random_pet" "orbit_mascot" {
   length = 2
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_caller_identity" "current" {}
+
+output "aws_account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
