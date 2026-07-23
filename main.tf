8978aba593d55fbca547ba1e63c655a156aee3c1
SMilewski <102833469+smilewski-personal@users.noreply.github.com>
2026-07-23 14:58:46 -0400

    Update main.tf to fix

--- main.tf
+++ main.tf
@@ -28,6 +28,7 @@ resource "aws_s3_bucket" "orbit_storage" {
     name        = "Orbit Labs Storage"
     managedBy   = "Spacelift"
     mission     = "First Launch"
+    project     = "Orbit-labs"
     environment = "demo"
   }
 }
